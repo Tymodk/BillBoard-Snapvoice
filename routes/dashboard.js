083c1036412f063d70dc3963e79fa04f3bf8da96
tymo.dekock <tymo.dekock@telenet.be>
2019-05-17 15:35:21 +0200

    working invoice buttons

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,13 +2,18 @@ const express = require("express");
 const Code = require('../models/code.model');
 const Connection = require('../models/connection.model');
 const User = require('../models/user.model');
+const Platform =require('../models/platform.model');
+
 const router = express.Router();
 
 // Display the dashboard page
 router.get("/", (req, res) => {
   User.findOne({_id: req.session.userId}, function (err, user) {  
     Code.findOne({userID: req.session.userId}, function (err, foundcode) {
-      Connection.find({userID: req.session.userId}, function (err, fconnections) {      
+      Connection.find({userID: req.session.userId}, function (err, fconnections) { 
+        for (let i = 0; i < fconnections.length; i++) {
+          const fconnection = fconnections[i];
+        }     
         if (err) console.log(err);
         console.log(foundcode);
         if(foundcode === null){
@@ -22,7 +27,6 @@ router.get("/", (req, res) => {
             res.render("dashboard", { code: foundcode.Code, connections:false, user:user});
           }else{
             res.render("dashboard", { code: foundcode.Code, connections:fconnections, user:user});
-
           }
         }
       });
